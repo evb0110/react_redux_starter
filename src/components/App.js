65f06b3996f1cb016188b6a88da6c12b1e0f410e
evb0110 <7149553@gmail.com>
2020-03-12 00:07:25 +0300

    starting the course

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+// https://www.udemy.com/course/react-redux/learn/lecture/12586858#overview
+// #163
+
+import React from 'react';
+import PostList from './PostList';
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <PostList/>
+        </div>
+    );
+};
+
+export default App;
